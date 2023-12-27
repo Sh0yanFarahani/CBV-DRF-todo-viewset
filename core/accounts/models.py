b8886611235325bbb1c437ea524e66b5d3cc46d4
shayan <137724478+Sh0yanFarahani@users.noreply.github.com>
2023-12-27 12:54:23 +0330

    build a custom user
    I used "AbstractBaseUser" to create a user cache and added the email.

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,3 +1,32 @@
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    PermissionsMixin,
+)
 from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .managers import CustomUserManager
 
-# Create your models here.
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_("email address"), max_length=255, unique=True)
+    
+    is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+
+    # with smtp fake server 
+    is_staff = models.BooleanField(default=False)
+    is_verified = models.BooleanField()
+
+    created_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
--- core/todo/models.py
+++ core/todo/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Todo(models.Model):
+    'create a model for todo app'
+    
+    title = models.CharField(max_length=245)
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
