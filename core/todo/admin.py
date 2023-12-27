5e52903542bffcb88efcfd655bf9c6bb9b2333b2
shayan <137724478+Sh0yanFarahani@users.noreply.github.com>
2023-12-27 13:03:35 +0330

    make a models for todo
    Create a model for Todo and add to the panel

--- core/todo/admin.py
+++ core/todo/admin.py
@@ -6,9 +6,13 @@ from django.contrib import admin
 from .models import Todo
 
 class TodoAdmin(admin.ModelAdmin):
+    model = Todo
     list_display = (
         'title',
         'body',
     )
+    list_filter = ('title', 'body',)
+    search_fields = ('title',)
+    ordering = ("title",)
 
-admin.site.register(Todo, )
+admin.site.register(Todo, TodoAdmin)
