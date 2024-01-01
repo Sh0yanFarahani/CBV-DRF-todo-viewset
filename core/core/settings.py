c64550d4de46895a740f0e9b5631378cd3b699b5
shayan <137724478+Sh0yanFarahani@users.noreply.github.com>
2024-01-01 22:53:46 +0330

    create file for api
    building folder for api and install rest api

--- core/core/settings.py
+++ core/core/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
-    'todo.apps.TodoConfig',    
+    'todo.apps.TodoConfig',   
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
